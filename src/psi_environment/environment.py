311c134e463e2befd1e581198440bb6babbca318
Krizzuu <47757676+Krizzuu@users.noreply.github.com>
2024-05-12 01:34:39 +0200

    added DummyAgent, added more notes about Environment.step() and get_timestep()

--- src/psi_environment/environment.py
+++ src/psi_environment/environment.py
@@ -40,14 +40,39 @@ class Environment:
                     self._edges[(y, x)] = deque()
                     self._edges[(x, y)] = deque()
 
-    def step(self, action) -> tuple[Any, float, bool]:
+    def step(self, action: int) -> tuple[Any, float, bool]:
         """
         Accepts action provided by agent when has to decide where to go. After that environment continues simulation
         and returns another observation of the environment and cost it took by taking the action.
-        :param action: direction where agent wants to go
+        :param action: an integer (in range from 0 to 2) of direction where the agent wants to go.
         :return: tuple of environment observation, cost of this action and bool stating if the agent has finished
         """
         # TODO: This requires a focus if only this will be returned.
         #  Also I'm not sure about how observation will look like.
+        #  How will be direction handled so it will be consistent. I suggest something like a clock
+        #  but it will require some extra checks of edges.
         #  And the implementation of it has not started
         raise NotImplementedError
+
+    def get_timestep(self) -> int:
+        """
+        Returns timestep of the environment
+        :return: timestep of the environment
+        """
+        return self._timestep
+
+
+class DummyAgent:
+    def __init__(self, parent_env: Environment, position: tuple[int, int], random_seed: int):
+        """
+        :param parent_env: environment in which this agent is placed (may be useful to get current timestep)
+        :param position: y, x coordinates of a dummy agent position
+        :param random_seed: seed that will be used to take actions
+        """
+        self._parent_env = parent_env
+        self._position = position
+        self._random_seed = random_seed
+
+    def get_action(self) -> int:
+        return (self._random_seed + self._parent_env.get_timestep()) % 3
+
